f469aefba7c878d71955ff916b3bbe9a6ebdc1d1
Sunghee Lee <630sunghee@naver.com>
2017-12-04 12:38:55 +0900

    routes/index

--- routes/index.js
+++ routes/index.js
@@ -25,20 +25,4 @@ router.get('/test', function(req, res, next) {
   res.render('test');
 });
 
-// router.post('/upload', uploadSetting.single('upload').function(req, res){
-//   var tmpPath = req.file.path;
-//   var fileName = req.file.filename;
-//   var newPath = "../public/images/" + fileName;
-//
-//   fs.rename(tmpPath, newPath, function(err){
-//     if(err){
-//       console.log(err);
-//     }
-//
-//     var html;
-//
-//     html="";
-//   })
-// })
-
 module.exports = router;
